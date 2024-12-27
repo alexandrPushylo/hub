d3d6cffcd7815b39c76b3469484c10c69bded3ef
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-12-27 08:38:57 +0300

    add assets

--- hub/assets.py
+++ hub/assets.py
@@ -1,6 +1,24 @@
+from .types import Enum
 CURRENCY_CHOICES = {
     'BYN': 'BYN',
     'USD': 'USD',
     'EUR': 'EUR',
     'RUB': 'RUB',
 }
+
+class Currency(Enum):
+    BYN = CURRENCY_CHOICES['BYN']
+    USD = CURRENCY_CHOICES['USD']
+    EUR = CURRENCY_CHOICES['EUR']
+    RUB = CURRENCY_CHOICES['RUB']
+
+
+class Bills(Enum):
+    ELECTRICITY = 'electricity'
+    COLD_WATER = 'cold_water'
+    HOT_WATER = 'hot_water'
+    RENT = 'rent'
+
+class Status(Enum):
+    OK = 'ok'
+    ERROR = 'error'
