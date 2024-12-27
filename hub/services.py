66902a382a44d3d108201b95430fb328821fad99
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-12-27 19:26:35 +0300

    fix: set_bills()

--- hub/services.py
+++ hub/services.py
@@ -49,23 +49,32 @@ def get_info_bills(bill: type[Electricity] | type[HotWater] | type[ColdWater] | 
 
 def set_bills(bill: type[Electricity] | type[HotWater] | type[ColdWater] | type[Rent], data: dict) -> bool:
     try:
+        payment_date = data.get('payment_date')
+        payment_date = U.datetime.strptime(payment_date, '%Y-%m-%d').date()
+        description = data.get('description')
+        indications = int(data.get('indications')) if data.get('indications') else None
+        rate = float(data.get('rate')) if data.get('rate') else None
+        amount = float(data.get('amount')) if data.get('amount') else None
+
+        diff_days = (U.TODAY() - payment_date).days
         last_bill = bill.objects.filter().order_by('payment_date').last()
-        if not last_bill:
-            return False
-        diff_days = (U.TODAY() - last_bill.payment_date).days
-        if U.TODAY().month > last_bill.payment_date.month or diff_days > 10:
-            print('add')
-            new_bill = bill.objects.create()
+
+        if not last_bill or U.TODAY().month > last_bill.payment_date.month or diff_days > 10:
+            log.debug('set_bills() - ADD')
+            new_bill = bill()
+            payment_date = U.TODAY()
         else:
-            print('edit')
+            log.debug('set_bills() - EDIT')
             new_bill = last_bill
         if new_bill:
-            new_bill.payment_date = data.get('payment_date')
-            new_bill.amount = data.get('amount')
-            new_bill.description = data.get('description')
+            new_bill.payment_date = payment_date
+            new_bill.description = description
             if bill not in (Rent,):
-                new_bill.indications = data.get('indications')
-                new_bill.rate = data.get('rate')
+                new_bill.indications = indications
+                new_bill.rate = rate
+                new_bill.amount = calculate_amount(rate, indications)
+            else:
+                new_bill.amount = amount
             new_bill.save()
         else:
             return False
