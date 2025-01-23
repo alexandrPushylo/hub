bf55f91f293206260b7b10a68fecc6f8c9b99054
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-01-23 20:22:00 +0300

    Update utilites.py

--- hub/utilites.py
+++ hub/utilites.py
@@ -1,9 +1,23 @@
 from datetime import date, datetime, timedelta
 
+from hub.models import Rent, Water, Electricity
+from hub.models import Debt, Debtor, ExchangeRate
+from hub.assets import MONTH_CHOICES
 
 from logger import getLogger
+
 log = getLogger(__name__)
 
 TODAY = date.today
 NOW = datetime.now().time
 MONTH = datetime.now().month
+
+
+def get_ru_month(month_eng: str) -> str:
+    for month in MONTH_CHOICES:
+        if month[0] == month_eng:
+            return month[1]
+
+def get_prev_month() -> str:
+    return MONTH_CHOICES[MONTH-2][0]
+
